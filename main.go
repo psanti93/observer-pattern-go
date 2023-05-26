4baa7cac54956b11201a4308844eaacd8c10b4b4
Paul Santiago <paulsantiago282@gmail.com>
2023-05-26 18:20:15 -0400

    observer-pattern in go first iteration

--- main.go
+++ main.go
@@ -1,10 +1,62 @@
 package main
 
+import "fmt"
+
+type Subject interface {
+	RegisterObserver(observer Observer)
+	NotifyObservers()
+}
+
+type Observer interface {
+	Update()
+}
+
+type Displace interface {
+	Display()
+}
+
+type WeatherData struct {
+	Observers   []Observer
+	Temperature int
+	Humidity    int
+	Pressure    int
+}
+
+func (w *WeatherData) RegisterObserver(observer Observer) {
+	w.Observers = append(w.Observers, observer)
+}
+
+func (w *WeatherData) NotifyObservers() {
+	for _, observer := range w.Observers {
+		observer.Update()
+	}
+}
+
+type CurrentDisplay struct {
+	WeatherData *WeatherData
+}
+
+func (c *CurrentDisplay) Display() {
+	fmt.Printf("The Current forecast for today is %d degrees with %d humidity", c.WeatherData.Temperature, c.WeatherData.Humidity)
+}
+
+func (c *CurrentDisplay) Update() {
+	c.Display()
+}
+
 func main() {
 
 	weatherData := &WeatherData{
 		Temperature: 60,
 		Humidity:    30,
+		Pressure:    20,
 	}
+
+	currentDisplay := &CurrentDisplay{
+		WeatherData: weatherData,
+	}
+
+	weatherData.RegisterObserver(currentDisplay)
+	weatherData.NotifyObservers()
 
 }
